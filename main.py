c4e6b942ce1a3be0b643e22fbb0b3cd72f2cfda0
Vladimir Demidov <uncojet@gmail.com>
2023-04-30 20:44:34 +0200

    add certs

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ if MODE == 'dev':
         app.run_polling()
 elif MODE == 'prod':
     def run(app):
-        app.start_webhook(listen="0.0.0.0", port=PORT, url_path=BOT_TOKEN)
+        # app.start_webhook(listen="0.0.0.0", port=PORT, url_path=BOT_TOKEN)
         # app.bot.run_webhook(f"{HEROKU_WEBHOOK_URL}{BOT_TOKEN}")
         app.run_webhook(
             listen="0.0.0.0",
